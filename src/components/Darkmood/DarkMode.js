c9ff8bb7209596e023b6510e9b40db720d696b32
Commatecs <121214628+Commatecs@users.noreply.github.com>
2023-12-14 13:30:51 +0200

    more edit

--- src/components/Darkmood/DarkMode.js
+++ src/components/Darkmood/DarkMode.js
@@ -2,33 +2,34 @@ import React from "react";
 import { ReactComponent as Sun } from "./Sun.svg";
 import { ReactComponent as Moon } from "./Moon.svg";
 import "./DarkMode.css";
+import { Switch } from "antd";
 
 export const DarkMode = () => {
-    
+
     const setDarkMode = () => {
         document.querySelector("body").setAttribute('data-theme', 'dark');
     }
 
-    const setLightMode =() => {
+    const setLightMode = () => {
         document.querySelector("body").setAttribute('data-theme', 'light');
-    }   
+    }
 
     const togglethem = e => {
-        if(e.target.checked)setDarkMode();
+        if (e.target.checked) setDarkMode();
         else setLightMode();
 
     };
     return (
         <div className='dark_mode'>
+            {/* <Switch checkedChildren={<Moon />} unCheckedChildren={<Sun />} defaultChecked /> */}
             <input
                 className='dark_mode_input'
                 type='checkbox'
                 id='darkmode-toggle'
                 onChange={togglethem}
             />
-            <label className='dark_mode_label' for='darkmode-toggle' style={{marginTop: '12px', marginRight: '10px'}}>
-                <Sun />
-                <Moon />
+            <label className='dark_mode_label' for='darkmode-toggle' style={{ marginTop: '12px', marginRight: '10px' }}>
+
             </label>
         </div>
     )
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './Project.css'
+import { Carousel } from 'antd'
 
 
 const list = [
@@ -18,33 +19,42 @@ const list = [
     },
     {
         name: "CooHosting",
-       description:"We will provide a free platform for teaching accounts and auditing to prepare new generations to be ready for the best job opportunity in the labor market ",
-        detail:"For Hosting Services",
+        description: "We will provide a free platform for teaching accounts and auditing to prepare new generations to be ready for the best job opportunity in the labor market ",
+        detail: "For Hosting Services",
         link: "https://coohosting.com/"
 
     },
     {
-        name:"ExaSpeed",
-        description:"ExaSpeed is a Team who Supports Companies on The IT Department by Consulting Contracts & Infrastructure of Data and Connections",
-        detail:"For The All IT Solutions",
+        name: "ExaSpeed",
+        description: "ExaSpeed is a Team who Supports Companies on The IT Department by Consulting Contracts & Infrastructure of Data and Connections",
+        detail: "For The All IT Solutions",
         link: "https://exaspeed-it.com/"
 
     },
     {
-        name:"Faathorh",
-        description:"e will provide a free platform for teaching accounts and auditing to prepare new generations to be ready for the best job opportunity in the labor market",
-        detail:"Faathorh Platform",
+        name: "Faathorh",
+        description: "e will provide a free platform for teaching accounts and auditing to prepare new generations to be ready for the best job opportunity in the labor market",
+        detail: "Faathorh Platform",
         link: "https://faatorh.com/"
 
     },
     {
-        name:"Madical mail",
-        description:"will be soon (We will announce the details later)",
-        detail:"Soon",
+        name: "Madical mail",
+        description: "will be soon (We will announce the details later)",
+        detail: "Soon",
         link: "#"
 
     },
 ]
+
+const contentStyle = {
+    margin: 0,
+    height: '160px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
 
 export const Project = () => {
     const [index, setIndex] = useState(0)
@@ -53,20 +63,37 @@ export const Project = () => {
     const change = (i) => {
         let idx = index
         idx += i
-        if(idx < 0)
-        {
-            idx = list.length -1
+        if (idx < 0) {
+            idx = list.length - 1
         }
-        else if(idx >= list.length)
-        {
+        else if (idx >= list.length) {
             idx = 0
         }
         setIndex(idx)
         setCurrent(list[idx])
     }
-  return (
-    <div id='project'>
-        
-    </div>
-  )
+    return (
+        <div id='project' className='carousel-projects'>
+            <Carousel autoplay>
+                <div className='carousel-item'>
+                    <div className='carousel-inner'>
+                        <h1>Commatecs</h1>
+                        <h2>Commatecs is a Software House</h2>
+                        <h3>Commatecs is a Software House We do all kind of Software Solutions Á& Artificial Intelligence (AI)</h3>
+                        <a>https://commatecs.com/</a>
+                        {/* <img src='https://i.pinimg.com/736x/2c/7a/76/2c7a7624c71d8f6bd44bd813035ee122.jpg' /> */}
+                    </div>
+                </div>
+                <div className='carousel-item'>
+                    <div className='carousel-inner'>
+                        <h1>CooTecStore</h1>
+                        <h2>Online Store (Soon)</h2>
+                        <h3>To provide the technology devices to serve the business community in the easiest, cheapest and most guaranteed way.</h3>
+                        <a>https://cotecstore.com/</a>
+                        {/* <img src='https://i.pinimg.com/736x/2c/7a/76/2c7a7624c71d8f6bd44bd813035ee122.jpg' /> */}
+                    </div>
+                </div>
+            </Carousel>
+        </div>
+    )
 }
